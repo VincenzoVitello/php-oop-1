aab33f3204896efb4ab7076ddba7bdf3adc7dae6
Your Name <you@example.com>
2022-02-25 16:22:57 +0100

    codice non funzionante

--- classes/Classes.php
+++ classes/Classes.php
@@ -2,22 +2,21 @@
 //creo nuova classe php
 class Film {
     //creo variabili private
-    private $poster;
+    public $poster;
     private $title;
     private $author;
     private $director;
     public $releaseDate;
     //creo i metodi che ci permetteranno di utilizzare queste variabili dentro index.php
     //il primo metodo sarà un costruttore, che renderà obbligatoria la dichiarazione di attributi da valorizzare. Il costruttore deve essere una function public
-    public function __construct($_poster, $_title, $_author, $_director){
-        $this->poster = $_poster;
+    public function __construct($_title, $_author, $_director){
         $this->title = $_title;
         $this->author = $_author;
         $this->director = $_director;
     }
     //adesso creo un metodo che mi permetterà di utilizzare i dati contenuti nelle varibili all'interno di index.php
     public function getFilm(){
-        return '{$this->poster}{$this->title}{$this->author}{$this->director}{$this->releaseDate}';
+        return '{$this->title}{$this->author}{$this->director}';
     }
 
 }
--- index.php
+++ index.php
@@ -10,28 +10,28 @@ require_once __DIR__.'/classes/Classes.php';
 //creo array in cui creo nuovi oggetti appartenenti alla classe inclusa
 $filmLibrary = [
     new Film [
-        'https://m.media-amazon.com/images/I/61rv+Rnsu6L._AC_.jpg',
+        'poster' => 'https://m.media-amazon.com/images/I/61rv+Rnsu6L._AC_.jpg',
         'The Avengers',
         'Joss Whedon',
         'Joss Whedon',
         'releaseDate' => '25 aprile 2012'
     ],
     new Film [
-        'https://www.lospaziobianco.it/wp-content/uploads/2015/02/avengers-poster-ae636.jpg',
+        'poster' => 'https://www.lospaziobianco.it/wp-content/uploads/2015/02/avengers-poster-ae636.jpg',
         'Avengers: Age of Ultron'
         'Joss Whedon',
         'Joss Whedon',
         'releaseDate' => '22 aprile 2015'
     ],
     new Film [
-        'https://m.media-amazon.com/images/I/71xu1PT1dwL._AC_SY606_.jpg',
+        'poster' => 'https://m.media-amazon.com/images/I/71xu1PT1dwL._AC_SY606_.jpg',
         'Avengers: Infinity War',
         'Christopher Markus e Stephen McFeely',
         'Anthony e Joe Russo',
         'releaseDate' => '25 aprile 2018'
     ],
     new Film [
-        'https://pad.mymovies.it/filmclub/2018/12/029/locandina.jpg',
+        'poster' => 'https://pad.mymovies.it/filmclub/2018/12/029/locandina.jpg',
         'The Avengers: Endgame',
         'Christopher Markus e Stephen McFeely',
         'Anthony e Joe Russo',
@@ -53,7 +53,9 @@ $filmLibrary = [
         <ul>
             <?php 
                 foreach($filmLibrary as $element){
-                    echo '<li>{element->getFilm()}</li>';
+                    echo "<li> <img src='{element['poster']}'></li>
+                          <li>{element->getFilm()}</li>
+                          <li>{element['releaseDate']}</li>";
                 }
             ?>
         </ul>
